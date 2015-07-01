8d32479c507b7ecee455017afc5f42bce2013ad9
Ramoth <ramothlp@gmail.com>
2015-07-01 09:53:48 +0200

    Create MyDealz Kategorien ausblenden.user.js

---
+++ MyDealz Kategorien ausblenden.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         MyDealz Kategorien ausblenden
+// @namespace    http://www.mydealz.de
+// @version      1.0
+// @description  Ausblendung der Kategorien
+// @author       Ramoth / vielleichtmann1
+// @include      http://www.mydealz.de/*
+// @grant        none
+// ==/UserScript==
+
+categories = document.getElementsByClassName("thread-category text--muted3 text--bold space--after-3");
+for (i=0; i<categories.length; i++)
+{
+categories[i].style.display = "none"; 
+}
